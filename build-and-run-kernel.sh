92b808a177a8b627f2ab905b45e341b3910e07ff
Andrew Paradi <me@andrewparadi.com>
2017-05-24 01:26:21 -0400

    Loop testing to log files

--- build-and-run-kernel.sh
+++ build-and-run-kernel.sh
@@ -2,11 +2,21 @@
 
 # runs OS/161 in SYS/161 and attaches GDB, side by side in a tmux window
 
+# set up bash to handle errors more aggressively - a "strict mode" of sorts
+set -e # give an error if any command finishes with a non-zero exit code
+set -u # give an error if we reference unset variables
+set -o pipefail # for a pipeline, if any of the commands fail with a non-zero exit code, fail the entire pipeline with that exit code
+
+cs350dir="/root/cs350-os161"
+sys161dir="/root/sys161"
+
 function show_help {
   echo "{ }   {default: builds from source, runs with gdb in Tmux}"
   echo "-b    {only build, don't run after}"
   echo "-r    {only run, don't build, don't run with gdb}"
   echo "-t {} {run test {testcode} }"
+  echo "-l    {loop test 100 times and log result}"
+  echo "-c    {clear all logs}"
   exit 0
 }
 
@@ -15,7 +25,8 @@ DEFAULT=true
 BUILD=false
 RUN=false
 TEST=false
-while getopts "h?:brt:" opt; do
+LOOP=false
+while getopts "h?:brlct:" opt; do
     case "$opt" in
     h|\?)
         show_help
@@ -29,6 +40,13 @@ while getopts "h?:brt:" opt; do
         RUN=true
         echo "Option Registered: run"
         ;;
+    l)  LOOP=true
+        echo "Option Registered: loop"
+        ;;
+    c)  rm $cs350dir/log/*.log
+        echo "Option Registered: clear logs"
+        exit 0
+        ;;
     t)  TEST=$OPTARG
         DEFAULT=false
         echo "Option Registered: test ${TEST}"
@@ -36,14 +54,6 @@ while getopts "h?:brt:" opt; do
         ;;
     esac
 done
-
-# set up bash to handle errors more aggressively - a "strict mode" of sorts
-set -e # give an error if any command finishes with a non-zero exit code
-set -u # give an error if we reference unset variables
-set -o pipefail # for a pipeline, if any of the commands fail with a non-zero exit code, fail the entire pipeline with that exit code
-
-cs350dir="/root/cs350-os161"
-sys161dir="/root/sys161"
 
 # display an error if we're not running inside a Docker container
 if ! grep docker /proc/1/cgroup -qa; then
@@ -109,23 +119,56 @@ function show_test_help {
   echo "### INVALID TEST CODE ###"
   echo "Use any of the following test codes in ./build-and-run.sh -t {test code}"
   echo "lock    l   {test locks with sy2}"
-  echo "convar  cv  {test conditional variables with sy2}"
+  echo "convar  cv  {test conditional variables with sy3}"
+  echo "traffic t   {A1 test for traffic simulation with 5 10 1 2 0 params}"
   exit 0
 }
 
-if [[ "$TEST" ]]; then
+if [[ "$TEST" != false ]]; then
+  log_ext=".log"
+  log_filename="`date '+%Y%m%d-%H%M%S'`-"
+
   start_test="Test ::"
+  test_command=""
+
   cd $cs350dir/root
+
   case $TEST in
     l|lock)     echo "${div} ${start_test} Lock ${div}"
-                sys161 kernel-$ASSIGNMENT "sy2;q"
+                test_command="sy2;q"
+                log_filename+="lock${log_ext}"
                 ;;
     cv|convar)  echo "${div} ${start_test} Conditional Variable ${div}"
-                sys161 kernel-$ASSIGNMENT "sy3;q"
+                test_command="sy3;q"
+                log_filename+="cond-var${log_ext}"
+                ;;
+    t|traffic)  echo "${div} ${start_test} A1 Traffic ${div}"
+                test_command="sp3 5 10 1 2 0;q"
+                log_filename+="traffic${log_ext}"
                 ;;
     *|h|\?)     show_test_help
                 exit 0
                 ;;
   esac
-  echo "${div} Test :: Finished ${div}"
+
+  if [[ "$LOOP" == true ]]; then
+    mkdir -p $cs350dir/log
+    logfile=$cs350dir/log/$log_filename
+    echo -n "1"
+    for i in {1..100}
+    do
+      [ $((i%2)) -eq 0 ] && echo -n "."
+      echo "${div} ${i} of 100 ${div}" >> $logfile
+      sys161 kernel-$ASSIGNMENT "${test_command}" &>> $logfile
+      echo "" >> $logfile
+    done
+    echo $i
+    success=$(grep -o "done" ${logfile} | wc -w)
+  else
+    sys161 kernel-$ASSIGNMENT "${test_command}"
+    i=1
+    success=1
+  fi
+
+  echo "${div} Test :: Finished ${success} / $i ${div}"
 fi
